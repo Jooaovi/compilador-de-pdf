6f2bd2bfb18dc774d91cb20c5bb8a9ea1bdb5cc5
joao victor <144910395+Jooaovi@users.noreply.github.com>
2025-12-06 18:16:27 -0300

    Add files via upload

---
+++ compilador de ar/compilador.py
@@ -0,0 +1,16 @@
+import PyPDF2
+import os
+
+merger = PyPDF2.PdfMerger()
+
+
+lista_arquivos = os.listdir("arquivos")
+lista_arquivos.sort()
+print(lista_arquivos)
+
+for arquivo in lista_arquivos:
+    if " .pdf" in arquivo:
+        merger.append("arquivo/{arquivo}")
+
+    merger.write("PDF final.pdf")
+        
